dc426e969e110ed6de864e6a275924c6a49c9e61
Daniil <dddedashev@mail.ru>
2021-09-20 21:15:06 +0300

    Edit ParcerTests 76% covered

--- hw1/hw1.tests/ParcerTests.cs
+++ hw1/hw1.tests/ParcerTests.cs
@@ -9,11 +9,37 @@ namespace hw1.tests
         [InlineData(new[] {"4", "-", "1"}, 0)]
         [InlineData(new[] {"3", "+", "1"}, 0)]
         
-        public void ParceRight_WhenArgumentsRight(string[] args, int expected)
+        public void ParseRight_WhenArgumentsRight(string[] args, int expected)
         {
             var res = Parser.TryToParse(args, out var val1,
                 out var operation, out var val2);
 
+            Assert.Equal(expected, res);
+        }
+        
+        [Theory]
+        [InlineData(new[] {"Gh", "+", "3"}, 1)]
+        [InlineData(new[] {"", "+", "a"}, 1)]
+        [InlineData(new[] {"aaaaaaa", "+", "ffffff"}, 1)]
+        [InlineData(new[] {"", "+", ""}, 1)]
+        public void ParseNotRight_AndReturnsOne(string[] args, int expected)
+        {
+            var actual = Parser.TryToParse(args, out var val1,
+                out var operation, out var val2);
+            
+            Assert.Equal(expected, actual);
+        }
+        
+        [Theory]
+        [InlineData(new[] {"777", "", "777"}, 2)]
+        [InlineData(new[] {"77", "ololo", "77"}, 2)]
+        [InlineData(new[] {"1","$","1"},2)]
+        [InlineData(new[] {"2"," ","3"},2)]
+        public void ParseNotRight_AndReturnsTwo(string[] args, int expected)
+        {
+            var res = Parser.TryToParse(args, out var val1,
+                out var operation, out var val2);
+            
             Assert.Equal(expected, res);
         }
     }
