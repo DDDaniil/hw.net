5261fc3d8cd39ef43e48ca34befd85f787e706a6
Daniil <dddedashev@mail.ru>
2021-09-20 21:00:22 +0300

    Add full CalculatorTests

--- hw1/hw1.tests/CalculatorTests.cs
+++ hw1/hw1.tests/CalculatorTests.cs
@@ -20,5 +20,17 @@ namespace hw1.tests
             Assert.Equal(expected, res);
             
         }
+        
+        [Theory]
+        [InlineData(777, "", 777, 0)]
+        [InlineData(77, "lol", 77, 0)]
+        [InlineData(7, "7", 7, 0)]
+        public void Calculate_IfArgumentsNotRight(int val1, string operation, 
+            int val2, int expected)
+        {
+            var res = Calculator.Calculate(val1, operation, val2);
+            
+            Assert.Equal(expected,res);
+        }
     }
 }
