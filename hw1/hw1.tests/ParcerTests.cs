5a5cc89223cda967fd3b96b04d5b79b6033c8148
Daniil <dddedashev@mail.ru>
2021-09-20 20:56:13 +0300

    Add some ParcerTests

---
+++ hw1/hw1.tests/ParcerTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace hw1.tests
+{
+    public class ParserTests
+    {
+        [Theory]
+        [InlineData(new[] {"1", "+", "2"}, 0)]
+        [InlineData(new[] {"4", "-", "1"}, 0)]
+        [InlineData(new[] {"3", "+", "1"}, 0)]
+        
+        public void ParceRight_WhenArgumentsRight(string[] args, int expected)
+        {
+            var res = Parser.TryToParse(args, out var val1,
+                out var operation, out var val2);
+
+            Assert.Equal(expected, res);
+        }
+    }
+}
