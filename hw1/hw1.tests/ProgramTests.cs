ee74c4038ea499182177b9f9ce9bffadfbe8f41c
Daniil <dddedashev@mail.ru>
2021-09-20 22:09:36 +0300

    Fixed and 100% covered

---
+++ hw1/hw1.tests/ProgramTests.cs
@@ -0,0 +1,28 @@
+﻿using Xunit;
+
+namespace hw1.tests
+{
+    public class ProgramTests
+    {
+        [Theory]
+        [InlineData(new[] {"777", "", "777"}, 2)]
+        [InlineData(new[] {"77", "ololo", "77"}, 2)]
+        [InlineData(new[] {"1","$","1"},2)]
+        [InlineData(new[] {"2"," ","3"},2)]
+        [InlineData(new[] {"Gh", "+", "3"}, 1)]
+        [InlineData(new[] {"", "+", "a"}, 1)]
+        [InlineData(new[] {"aaaaaaa", "+", "ffffff"}, 1)]
+        [InlineData(new[] {"", "+", ""}, 1)]
+        [InlineData(new[] {"1", "+", "2"}, 0)]
+        [InlineData(new[] {"4", "-", "1"}, 0)]
+        [InlineData(new[] {"3", "+", "1"}, 0)]
+        public void CorrectionProgram(string[] args, int expected)
+        {
+            /*var res = Parser.TryToParse(args, out var val1,
+                out var operation, out var val2);*/
+            var res = Program.Main(args);
+            
+            Assert.Equal(expected, res);
+        }
+    }
+}
