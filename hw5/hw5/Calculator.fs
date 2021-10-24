9c328297a1a0c79197539e80498e052ea2410f88
Daniil <dddedashev@mail.ru>
2021-10-24 22:39:08 +0300

    First

---
+++ hw5/hw5/Calculator.fs
@@ -0,0 +1,10 @@
+﻿module hw5.Calculator
+
+    let Calculate val1 operation val2 =
+        let a = val1 |> double
+        let b = val2 |> double
+        match operation with
+        | "+" -> a + b
+        | "-" -> a - b
+        | "*" -> a * b
+        | "/" -> a / b
