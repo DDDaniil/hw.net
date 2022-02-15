f961513e9f1d1b1d700b9a8ec1d800bc5f7958a7
Daniil <dddedashev@mail.ru>
2022-02-15 13:04:16 +0300

    edit

---
+++ hw5/hw1/hw1/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace hw1
+{
+    public static class Calculator
+    {
+        public static double Calculate(double val1, string operation, double val2)
+        {
+            double result = 0;
+            result = operation switch
+            {
+                "+" => val1 + val2,
+                "-" => val1 - val2,
+                "*" => val1 * val2,
+                "/" => val1 / val2,
+                _ => result
+            };
+            return result;
+        }
+    }
+}
